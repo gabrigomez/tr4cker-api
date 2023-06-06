8e3e9ab35313390974c525af0bbc737c256cbec3
Gabriel <gabrigomez15@gmail.com>
2023-06-06 15:16:56 -0300

    feat: add restframework on installed apps

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+class User(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30)
+    email = models.CharField(unique=True)
+    password = models.CharField()
