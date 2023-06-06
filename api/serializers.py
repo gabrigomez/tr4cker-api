5a8fa7817ba636df244bd08739f432ce09ec5214
Gabriel <gabrigomez15@gmail.com>
2023-06-06 15:27:15 -0300

    feat: register api in settings installed apps and create views and serializers for User

---
+++ api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'password')
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'username')
--- api/views.py
+++ api/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from rest_framework import generics
+
+from .models import User
+from .serializers import UserSerializer
 
 
 def index(request):
     return HttpResponse("Hello, Server!")
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
