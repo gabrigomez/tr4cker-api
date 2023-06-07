d31593c930291711a890f3990dc8b7f0c99cc3dd
Gabriel <gabrigomez15@gmail.com>
2023-06-07 10:16:20 -0300

    feat: add update user test

--- api/test_api.py
+++ api/test_api.py
@@ -3,14 +3,13 @@ from faker import Faker
 
 # Create your tests here.
 ENDPOINT = 'http://127.0.0.1:8000/api'
+fake = Faker()
 
 def test_run_server():
     response = requests.get(ENDPOINT)
     assert response.status_code == 200
 
 def test_can_post_user():
-    fake = Faker()
-
     email:str = fake.email()
     username = email.split('@')
     password = fake.password(length=6, special_chars=False)
@@ -21,14 +20,26 @@ def test_can_post_user():
         'password': password
     }
 
-    print(payload)
+    # print(payload)
     response = requests.post(ENDPOINT + '/register', json=payload)
     assert response.status_code == 201
 
 def test_can_get_all_users():
     response = requests.get(ENDPOINT + '/users')
     assert response.status_code == 200
-    
+
+def test_can_update_user():
+    username = fake.email().split('@')[0]
+    password = fake.password(length=6, special_chars=False)
+
+    payload = {
+        'username': username,        
+        'password': password
+    }
+
+    response = requests.put(ENDPOINT + '/user/2', json=payload)
+    assert response.status_code == 200
+
 
 
 
