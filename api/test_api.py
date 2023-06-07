908d572db6d128286f8cc608e9926435e5ea627e
Gabriel <gabrigomez15@gmail.com>
2023-06-07 11:09:44 -0300

    feat: add get token test

--- api/test_api.py
+++ api/test_api.py
@@ -1,13 +1,28 @@
-import requests
+import requests, os, dotenv
 from faker import Faker
 
 # Create your tests here.
 ENDPOINT = 'http://127.0.0.1:8000/api'
 fake = Faker()
+token = ''
+
+dotenv.load_dotenv(dotenv.find_dotenv())
 
 def test_run_server():
     response = requests.get(ENDPOINT)
     assert response.status_code == 200
+
+def test_can_obtain_token():    
+    payload = {
+        'username': os.getenv("admin"),
+        'password': os.getenv("admin_password")
+    }
+    
+    response = requests.post(ENDPOINT + '/token', json=payload)
+    data = response.json()
+    token = data.get('access')
+    
+    assert response.status_code == 200    
 
 def test_can_post_user():
     email:str = fake.email()
