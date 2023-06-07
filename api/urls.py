328f8b41bc4ac14b4c29dcc704c6a023b8315129
Gabriel <gabrigomez15@gmail.com>
2023-06-07 09:56:49 -0300

    fix: token route

--- api/urls.py
+++ api/urls.py
@@ -10,7 +10,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('', views.index),
     path('register', RegisterUserView.as_view()),
-    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('users', UsersListView.as_view()),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),    
 ]
